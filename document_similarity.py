7c6711bbe961258f7b6e078c313fa775505c1ad8
dmvarela <146129101+dmvarela@users.noreply.github.com>
2023-10-18 10:20:29 -0600

    Create document_similarity.py

---
+++ document_similarity.py
@@ -0,0 +1,11 @@
+import glob
+import os
+file_list = glob.glob(os.path.join(os.getcwd(), "zone/text_mining/document_similarity", "*.txt"))
+
+raw_documents = []
+
+for file_path in file_list:
+    with open(file_path, encoding="utf8") as f_input:
+        raw_documents.append(f_input.read())
+
+print("Number of documents:",len(raw_documents))
